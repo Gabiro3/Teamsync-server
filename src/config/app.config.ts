6b79602bc8dc7ca791e7051f76711de2198edb26
Gabiro Arnaud <gabiroarnaud@gmail.com>
2025-01-27 10:13:13 +0200

    Err fix

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,9 +1,9 @@
 import { getEnv } from "../utils/get-env";
 
 const appConfig = () => ({
-  NODE_ENV: getEnv("NODE_ENV", "production"),
-  PORT: getEnv("PORT", "5000"),
-  BASE_PATH: getEnv("BASE_PATH", "/api"),
+  NODE_ENV: getEnv("NODE_ENV"),
+  PORT: getEnv("PORT"),
+  BASE_PATH: getEnv("BASE_PATH"),
   MONGO_URI: getEnv("MONGO_URI", ""),
 
   SESSION_SECRET: getEnv("SESSION_SECRET"),
@@ -13,7 +13,7 @@ const appConfig = () => ({
   GOOGLE_CLIENT_SECRET: getEnv("GOOGLE_CLIENT_SECRET"),
   GOOGLE_CALLBACK_URL: getEnv("GOOGLE_CALLBACK_URL"),
 
-  FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "localhost"),
+  FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN"),
   FRONTEND_GOOGLE_CALLBACK_URL: getEnv("FRONTEND_GOOGLE_CALLBACK_URL"),
 
   RESEND_API_KEY: getEnv("RESEND_API_KEY")
